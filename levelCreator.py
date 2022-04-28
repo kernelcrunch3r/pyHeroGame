2e5fc7efe8fe1b22582b6f88b32f5df9b991f5c8
kernelcrunch3r <curtispvos@gmail.com>
2022-04-28 14:53:47 -0400

    finished the level creator

--- levelCreator.py
+++ levelCreator.py
@@ -1,10 +1,39 @@
 import time
 import keyboard
 
-getTime = time.
+start_time = time.time()
+print(start_time)
 
-level = open("testLevel.txt", "w")
+name = input("Song: ")
 
+level = open("{}.txt".format(name), "w")
+
+was_pressed = False  # use to make sure no keys are held
 while True:
     if keyboard.is_pressed("a"):
-        getTime
+        if not was_pressed:  # only change file if the
+            level.writelines("{}, {}\n".format("a", time.time() - start_time))
+            was_pressed = True  # set to true so that even if key is pressed, nothing will happen; no holding
+    elif keyboard.is_pressed("f"):
+        if not was_pressed:
+            level.writelines("{}, {}\n".format("f", time.time() - start_time))
+            was_pressed = True  # set to true so that even if key is pressed, nothing will happen; no holding
+    elif keyboard.is_pressed("j"):
+        if not was_pressed:
+            level.writelines("{}, {}\n".format("j", time.time() - start_time))
+            was_pressed = True  # set to true so that even if key is pressed, nothing will happen; no holding
+    elif keyboard.is_pressed(";"):
+        if not was_pressed:
+            level.writelines("{}, {}\n".format(";", time.time() - start_time))
+            was_pressed = True  # set to true so that even if key is pressed, nothing will happen; no holding
+    elif keyboard.is_pressed("space"):
+        if not was_pressed:
+            level.writelines("{}, {}\n".format("space", time.time() - start_time))
+            was_pressed = True  # set to true so that even if key is pressed, nothing will happen; no holding
+    else:
+        was_pressed = False  # reset once the key is released
+
+    if keyboard.is_pressed("q"):
+        break
+
+level.close()
