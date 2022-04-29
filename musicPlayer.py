a9531257ccbb3a92618cda3a16b9831d81c08d4f
kernelcrunch3r <curtispvos@gmail.com>
2022-04-29 14:54:53 -0400

    Update musicPlayer.py

--- musicPlayer.py
+++ musicPlayer.py
@@ -2,7 +2,8 @@ import pygame
 
 name = input("What song to open: ")
 with open("songs\\{}".format(name)) as file:
-    notes = list(file.readlines())
+    notes = list(file.read().split("\n"))  # get each note and its time in an element of an array
     file.close()
 
 print(notes)
+
